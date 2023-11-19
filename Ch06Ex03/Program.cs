de51964a57ff20597f9b731c6a3fa50100485234
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-19 16:29:37 +0100

    Add project files.

---
+++ Ch06Ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ch06Ex03
+{
+    internal class Program
+    {
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum = SumVals(1, 5, 2, 9, 8);
+            Console.WriteLine($"Summed values: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
